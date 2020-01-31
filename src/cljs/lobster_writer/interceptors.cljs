8f8f6d9a9521bb2688bd44818c44216d6bae870e
Dave Martin <david.martin@fundingcircle.com>
2020-01-31 16:41:05 +0000

    Don't save nil db

--- src/cljs/lobster_writer/interceptors.cljs
+++ src/cljs/lobster_writer/interceptors.cljs
@@ -5,7 +5,7 @@
   (re-frame.core/->interceptor
     :id :persist-app-db
     :after (fn [context]
-               (let [db (get-in context [:effects :db])]
+               (when-let [db (get-in context [:effects :db])]
                  (js/localStorage.setItem constants/local-storage-app-db-key (prn-str db))
                  (-> context
                      (assoc-in [:effects :lobster-writer.effects/show-saving-indicator] true)
